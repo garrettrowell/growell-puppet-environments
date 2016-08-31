9a2ab639c51a219edda5b085cf088d851e69bed8
Garrett Rowell <garrett2@cnwr.com>
2016-08-31 13:14:21 -0400

    setup basic environment

---
+++ manifests/site.pp
@@ -0,0 +1,11 @@
+node 'growell-katello-master.dev.cnwr.com' {
+  include ::roles::common
+}
+
+node 'growell-openstack-allinone.dev.cnwr.com' {
+  include ::roles::common
+}
+
+node 'growell-puppetdb.dev.cnwr.com' {
+  include ::roles::common
+}
